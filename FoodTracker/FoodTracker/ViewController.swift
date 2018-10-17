63ca5f1c1afd22f4601aaa8662fddb23bf1ee5e8
erguerra <42151534+erguerra@users.noreply.github.com>
2018-10-17 17:44:01 -0300

    initial commit (#1)
    * initial commit
    
    * Primeira tarefa

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Eduardo Guerra on 17/10/18.
+//  Copyright © 2018 Eduardo Guerra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

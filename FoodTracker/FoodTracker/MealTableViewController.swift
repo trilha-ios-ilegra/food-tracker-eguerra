e21e0d4b3edacb486452527145f05b1deca7a5d5
erguerra <42151534+erguerra@users.noreply.github.com>
2018-10-23 14:08:19 -0300

    Inserts a new scene with the table view of meals (#7)
    * Inserts a new scene with the table view of meals and a table view controller to menage the new scene
    
    * removes unnecessary methods and comments

---
+++ FoodTracker/FoodTracker/MealTableViewController.swift
@@ -0,0 +1,80 @@
+//
+//  MealTableViewController.swift
+//  FoodTracker
+//
+//  Created by Eduardo Guerra on 23/10/18.
+//  Copyright © 2018 Eduardo Guerra. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewController: UITableViewController {
+    
+    //MARK: Properties
+    
+    var meals = [Meal]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Load the sample data.
+        loadSampleMeals()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+       
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return meals.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "MealTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+        
+        // Fetches the appropriate meal for the data source layout.
+        let meal = meals[indexPath.row]
+        
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+
+        return cell
+    }
+    
+    //MARK: Private Methods
+    
+    private func loadSampleMeals() {
+        let photo1 = UIImage(named: "First Meal ImSet")
+        let photo2 = UIImage(named: "Second Meal ImSet")
+        let photo3 = UIImage(named: "Third Meal ImSet")
+        
+        guard let meal1 = Meal(name: "Carrots and Meat", photo: photo1, rating: 4) else {
+            fatalError("Unable to instantiate meal1")
+        }
+        
+        guard let meal2 = Meal(name: "Fitness Stuffed Pancakes", photo: photo2, rating: 5) else {
+            fatalError("Unable to instantiate meal2")
+        }
+        
+        guard let meal3 = Meal(name: "Salmon and Baked Tomatoes", photo: photo3, rating: 3) else {
+            fatalError("Unable to instantiate meal3")
+        }
+        
+        meals += [meal1, meal2, meal3]
+        
+        
+        
+    }
+
+}

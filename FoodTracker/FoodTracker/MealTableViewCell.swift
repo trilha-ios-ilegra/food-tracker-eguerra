e71e5d9b50b6ab2f0fb9714fb35412e1547dc942
erguerra <42151534+erguerra@users.noreply.github.com>
2018-10-25 14:20:41 -0300

    Release Version 1.0.0 (#11)
    * initial commit (#1)
    
    * initial commit
    
    * Primeira tarefa
    
    * Connect text fields to code
    
    Outlets creation for all the subviews of the scene, action added to the setDefaultTextButton and delegate protocol implementation by the ViewController class
    
    * Add the add a meal photo functionality along with the default picture (#3)
    
    * first rating button added (#4)
    
    * first rating button added
    
    * firs rating button added with the correct size
    
    * Add the rating functionality by adding five star buttons.
    
    * refactoring of the setupButtons function
    
    * will fix complexity of the updateButtonState function
    
    * Refactor: Removing addButtonConstraints and setButtonImages methods and creating UIButton extensions for replacing those methods
    
    * Feature/define your data model (#5)
    
    * first rating button added
    
    * firs rating button added with the correct size
    
    * Add the rating functionality by adding five star buttons.
    
    * refactoring of the setupButtons function
    
    * will fix complexity of the updateButtonState function
    
    * Refactor: Removing addButtonConstraints and setButtonImages methods and creating UIButton extensions for replacing those methods
    
    * insertion of unit tests and a Meal model class
    
    * Fix on the curly brackets convention in the header of the Meal class.
    
    * first rating button added
    
    * firs rating button added with the correct size
    
    * Add the rating functionality by adding five star buttons.
    
    * insertion of unit tests and a Meal model class
    
    * Fix on the curly brackets convention in the header of the Meal class.
    
    * Update RatingControl.swift (#6)
    
    fix merge error
    
    * Inserts a new scene with the table view of meals (#7)
    
    * Inserts a new scene with the table view of meals and a table view controller to menage the new scene
    
    * removes unnecessary methods and comments
    
    * adds navigation to the scenes and the add a meal functionality (#8)
    
    * adds navigation to the scenes and the add a meal functionality
    
    * Fixes the exit navigation of the Save bar button
    
    * adds the edit and delete meals from the table view (#9)
    
    * adds the edit and delete meals from the table view
    
    * fixes complexity of the prepare method from the MealTableViewController class
    
    * Persist Data (#10)
    
    * add the persist data functionality
    
    * fix identation of the Meal class
    
    * Bump version to 1.0.0

---
+++ FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Eduardo Guerra on 23/10/18.
+//  Copyright © 2018 Eduardo Guerra. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
